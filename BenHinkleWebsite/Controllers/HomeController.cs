a437b58cf1f4654a65554fc244c91706f5a9ebce
Ben <bjhinkle22@gmail.com>
2019-02-16 21:12:04 +0000

    fixes

--- BenHinkleWebsite/Controllers/HomeController.cs
+++ BenHinkleWebsite/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BenHinkleWebsite.Controllers
+namespace MyWebsite.Controllers
 {
     public class HomeController : Controller
     {
@@ -15,14 +15,14 @@ namespace BenHinkleWebsite.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "I am learning to code each day more and more by doing simple projects at home.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Feel free to reach me anytime, or connect with me on LinkedIn.";
 
             return View();
         }
